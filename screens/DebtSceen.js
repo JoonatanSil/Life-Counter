83a10bc399cbcc7cb92bd71802f0037788e7aef2
Jesse Friberg <jesse.friberg@edu.turkuamk.fi>
2022-11-12 14:34:15 +0200

    styling

--- screens/DebtSceen.js
+++ screens/DebtSceen.js
@@ -20,27 +20,9 @@ const CounterScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "black",
-        paddingHorizontal: windowWidth * 0.05,
-        paddingTop: windowHeight * 0.1,
-    },
-    header: {
-        flex: 1,
-        flexDirection: "row",
-    },
-    headerText: {
-        color: "white",
-        fontWeight: "bold",
-        fontSize: 36
-    },
-    menuButton: {
-        paddingHorizontal: windowWidth * 0.05,
-        //paddingTop: windowHeight * 0.10,
-    },
-    menuButtonText: {
-        paddingHorizontal: windowWidth * 0.05,
-        //paddingTop: windowHeight * 0.10,
-        color: "white",
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
     },
 });
 export default CounterScreen;
