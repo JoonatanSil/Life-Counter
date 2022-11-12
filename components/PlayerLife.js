0ab4fe915c903f41f575fa826ab1fd546385cc4b
santeri.salminen <santeri.salminen.99@gmail.com>
2022-11-12 13:17:51 +0200

    Added life variable & setter function

--- components/PlayerLife.js
+++ components/PlayerLife.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 const PlayerLife = (props) => {
+    const [life, setLife] = useState(40);
+
     return (
         <View style={{ ...styles.playerCounter, borderColor: props.color }}>
-            <Text style={{ ...styles.lifeAmount, color: props.color }}>
-                40
-            </Text>
+            <TouchableOpacity style={styles.lifeButton} onPress={() => setLife((prevLife) => prevLife - 1)}>
+                <Text style={{ ...styles.lifeAmount, color: props.color }}>
+                    {life}
+                </Text>
+            </TouchableOpacity>
         </View>
     )
 }
@@ -23,6 +27,11 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         marginHorizontal: windowWidth * 0.01,
         marginVertical: windowHeight * 0.01,
+    },
+    lifeButton: {
+        width: "100%",
+        height: "100%",
+        justifyContent: "center",
     },
     lifeAmount: {
         color: "green",
