363044c64e48897aa9d89180a136bd15e24dc2bd
Henri Kuoppala <henri.kuoppala@edu.turkuamk.fi>
2022-11-13 20:05:01 +0200

    stuff

--- components/DebtListItem.js
+++ components/DebtListItem.js
@@ -1,30 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
 
 const MenuItem = (props) => {
-    return (
-        <View style={styles.container}>
-            <Text style={{ color: "white", marginRight: 70 }}>
-                Player name here -
-            </Text>
-            <TouchableOpacity style={styles.debtButton}>
-                <Text style={{ color: "white" }}>6,90€</Text>
-            </TouchableOpacity>
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <Text style={{ color: 'white', marginRight: 70 }}>Pelaajan nimi -</Text>
+      <TouchableOpacity style={styles.debtButton}>
+        <Text style={{ color: 'black' }}>5,20£</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: "row",
-        margin: 10,
-        alignItems: "center"
-    },
-    debtButton: {
-        backgroundColor: "#0060A6",
-        borderRadius: 10,
-        padding: 10
-    },
-
+  container: {
+    flexDirection: 'row',
+    margin: 10,
+    alignItems: 'center',
+  },
+  debtButton: {
+    backgroundColor: '#008899',
+    borderRadius: 2,
+    padding: 8,
+  },
 });
 
 export default MenuItem;
