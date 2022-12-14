4508c0bc9ca6a35a1ebcf0e83954df3864624174
santeri.salminen <santeri.salminen.99@gmail.com>
2022-12-14 12:39:19 +0200

    Players can now plus and minus life

--- components/PlayerLife.js
+++ components/PlayerLife.js
@@ -1,43 +1,47 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from "react-native";
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 const PlayerLife = (props) => {
-    const [life, setLife] = useState(40);
+  const [life, setLife] = useState(40);
 
-    return (
-        <View style={{ ...styles.playerCounter, borderColor: props.color }}>
-            <TouchableOpacity style={styles.lifeButton} onPress={() => setLife((prevLife) => prevLife - 1)}>
-                <Text style={{ ...styles.lifeAmount, color: props.color }}>
-                    {life}
-                </Text>
-            </TouchableOpacity>
-        </View>
-    )
-}
+  return (
+    <View style={{ ...styles.playerCounter, borderColor: props.color }}>
+      <TouchableOpacity
+        style={styles.lifeButton}
+        onPress={() => setLife((prevLife) => prevLife + 1)}
+      ></TouchableOpacity>
+      <Text style={{ ...styles.lifeAmount, color: props.color }}>{life}</Text>
+      <TouchableOpacity
+        style={styles.lifeButton}
+        onPress={() => setLife((prevLife) => prevLife - 1)}
+      ></TouchableOpacity>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
-    playerCounter: {
-        borderWidth: 6,
-        borderRadius: 20,
-        borderColor: "green",
-        width: windowWidth * 0.43,
-        height: windowHeight * 0.43,
-        justifyContent: "center",
-        marginHorizontal: windowWidth * 0.01,
-        marginVertical: windowHeight * 0.01,
-    },
-    lifeButton: {
-        width: "100%",
-        height: "100%",
-        justifyContent: "center",
-    },
-    lifeAmount: {
-        color: "green",
-        fontWeight: "bold",
-        fontSize: windowWidth * 0.17,
-        textAlign: "center",
-    },
+  playerCounter: {
+    borderWidth: 6,
+    borderRadius: 20,
+    borderColor: "green",
+    width: windowWidth * 0.43,
+    height: windowHeight * 0.43,
+    justifyContent: "center",
+  },
+  lifeButton: {
+    width: "100%",
+    height: "50%",
+    justifyContent: "center",
+  },
+  lifeAmount: {
+    color: "green",
+    fontWeight: "bold",
+    fontSize: windowWidth * 0.17,
+    textAlign: "center",
+    position: "absolute",
+    alignSelf: "center",
+  },
 });
 export default PlayerLife;
--- screens/CounterScreen.js
+++ screens/CounterScreen.js
@@ -1,52 +1,44 @@
-import React from 'react';
-import { StyleSheet, SafeAreaView, Text, View, Dimensions, TouchableOpacity } from 'react-native';
-import PlayerLife from '../components/PlayerLife';
+import React from "react";
+import { StyleSheet, SafeAreaView, Text, View, Dimensions, TouchableOpacity } from "react-native";
+import PlayerLife from "../components/PlayerLife";
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 const CounterScreen = ({ navigation }) => {
-    return (
-        <SafeAreaView style={styles.container}>
-            <TouchableOpacity style={styles.menuButton} onPress={() => navigation.navigate("Menu")}>
-                <Text style={styles.menuButtonText}>Menu</Text>
-            </TouchableOpacity>
-            <View style={styles.lifeContainer}>
-                <PlayerLife
-                    color="green"
-                />
-                <PlayerLife
-                    color="blue"
-                />
-                <PlayerLife
-                    color="red"
-                />
-                <PlayerLife
-                    color="yellow"
-                />
-            </View>
-        </SafeAreaView>
-    )
-}
+  return (
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity style={styles.menuButton} onPress={() => navigation.navigate("Menu")}>
+        <Text style={styles.menuButtonText}>Menu</Text>
+      </TouchableOpacity>
+      <View style={styles.lifeContainer}>
+        <PlayerLife color="green" />
+        <PlayerLife color="blue" />
+        <PlayerLife color="red" />
+        <PlayerLife color="yellow" />
+      </View>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexWrap: "wrap",
-        backgroundColor: "black",
-    },
-    menuButton: {
-        paddingHorizontal: windowWidth * 0.05,
-        paddingTop: windowHeight * 0.10,
-    },
-    lifeContainer: {
-        flex: 1,
-        flexWrap: "wrap",
-        paddingHorizontal: windowWidth * 0.05,
-    },
-    menuButtonText: {
-        color: "white",
-    },
+  container: {
+    flex: 1,
+    flexWrap: "wrap",
+    backgroundColor: "black",
+  },
+  menuButton: {
+    paddingHorizontal: windowWidth * 0.05,
+    paddingTop: windowHeight * 0.1,
+  },
+  lifeContainer: {
+    flex: 1,
+    flexWrap: "wrap",
+    paddingHorizontal: windowWidth * 0.05,
+  },
+  menuButtonText: {
+    color: "white",
+  },
 });
 
 export default CounterScreen;
